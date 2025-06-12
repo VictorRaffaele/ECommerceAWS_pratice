13865d76f928871b51fff498e72230c0a5a78cae
VictorRaffaele <victorraffaele@gmail.com>
2025-06-12 13:48:51 -0300

    chore: log request id

--- lambda/products/productsFecthFunction.ts
+++ lambda/products/productsFecthFunction.ts
@@ -4,13 +4,14 @@ const funcTag = "ProductsFetchFunction";
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   // receive from APIGateway an event and return from APIGateway a response
 
-  console.log(funcTag + " Received event:", JSON.stringify(event, null, 2));
-  console.log(funcTag + " Context:", JSON.stringify(context, null, 2));
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext?.requestId || "N/A";
 
+  console.log(`${funcTag} API Gateway RequestId: ${lambdaRequestId} - Lambda RequestId: ${apiRequestId}`);
   const method = event.httpMethod;
   if (event.resource === "/products") {
     if (method === 'GET') {}
-      console.log(funcTag + " GET /products called");
+      console.log(`${funcTag} GET /products called`);
 
       return {
         statusCode: 200,

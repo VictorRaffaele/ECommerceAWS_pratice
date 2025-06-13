0040d4c1144201b21d567050832b6dfb5be3d999
VictorRaffaele <victorraffaele@gmail.com>
2025-06-13 16:15:09 -0300

    feat: create dinamoDB table for products

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,15 +1,31 @@
 import * as lamdba from 'aws-cdk-lib/aws-lambda';
 import * as lamdbaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class ProductsAppStack extends cdk.Stack {
   // Config stack defines the Lambda function for fetching products
   readonly productsFetchHandler: lamdbaNodeJS.NodejsFunction;
+  readonly productsDdb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Define the DynamoDB table for products
+    this.productsDdb = new dynamodb.Table(this, 'ProductsDdb', {
+      tableName: 'Products',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Used DESTROY for development purposes
+      partitionKey: {
+        name: 'productId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // On-demand billing mode
+      readCapacity: 1,
+      writeCapacity: 1
+    });
+
+    // Define the Lambda function that fetches products from DynamoDB
     this.productsFetchHandler = new lamdbaNodeJS.NodejsFunction(this, 
         'ProductsFetchHandler', {
         runtime: lamdba.Runtime.NODEJS_20_X,
@@ -22,6 +38,12 @@ export class ProductsAppStack extends cdk.Stack {
           minify: true,
           sourceMap: false,
         },
-      })
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
+      }
+    );
+
+    this.productsDdb.grantReadData(this.productsFetchHandler);
   }
 }

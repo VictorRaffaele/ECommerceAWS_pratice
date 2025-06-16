dac9ed3781e736b9a8f6ddda532fb92059f78d9e
VictorRaffaele <victorraffaele@gmail.com>
2025-06-16 14:19:51 -0300

    feat: update functions to use Layer

--- lambda/products/ProductsAdminFunction.ts
+++ lambda/products/ProductsAdminFunction.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from 'aws-sdk';
 
+const productsDdb = process.env.PRODUCTS_DDB || "";
+const ddbClient = new DynamoDB.DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 const funcTag = "ProductsAdminFunction";
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   // receive from APIGateway an event and return from APIGateway a response
@@ -12,11 +17,15 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
   if (event.resource === "/products") {
     if (event.httpMethod === 'POST') {
       console.log(`${funcTag} POST /products called`);
+      const product = JSON.parse(event.body!) as Product;
+      const productCreated = await productRepository.createProduct(product);
 
       return {
         statusCode: 201,
         body: JSON.stringify({
-          message: "Product created successfully",
+          product: productCreated,
+          message: `Product with id: ${productCreated.productId} created successfully`,
+          error: null
         })
       };
     };
@@ -24,15 +33,51 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
     const productId = event.pathParameters!.id as string;
     if (event.httpMethod === 'PUT') {
       console.log(`${funcTag} PUT /products/${productId} called`);
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Product with id: ${productId} updated successfully`,
-        })
-      };
+      const product = JSON.parse(event.body!) as Product;
+      try {
+        const productUpdated = await productRepository.updateProduct(productId, product);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            product: productUpdated,
+            message: `Product with id: ${productId} updated successfully`,
+            error: null
+          })
+        };
+      } catch (ConditionalCheckFailedException) {
+        console.error(`${funcTag} Error updating product: Not Found`);
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            product: null,
+            message: `Product not found`,
+            error: 'Not Found'
+          })
+        };
+      }
     } else if (event.httpMethod === 'DELETE') {
       console.log(`${funcTag} DELETE /products/${productId} called`);
+      try {
+        const product = await productRepository.deleteProduct(productId);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            product,
+            message: `Product with id: ${productId} deleted successfully`,
+            error: null
+          })
+        };
+      } catch (error) {
+        console.error(`${funcTag} Error deleting product:`, (<Error>error).message);
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            product: null,
+            message: `Error deleting product with id: ${productId}`,
+            error: (<Error>error).message
+          })
+        };
+      }
 
       return {
         statusCode: 200,
--- lambda/products/productsFecthFunction.ts
+++ lambda/products/productsFecthFunction.ts
@@ -1,6 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { DynamoDB } from 'aws-sdk';
+import { error } from "console";
 
+const productsDdb = process.env.PRODUCTS_DDB || "";
+const ddbClient = new DynamoDB.DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDdb);
 const funcTag = "ProductsFetchFunction";
+
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   // receive from APIGateway an event and return from APIGateway a response
 
@@ -9,33 +16,43 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
 
   console.log(`${funcTag} API Gateway RequestId: ${lambdaRequestId} - Lambda RequestId: ${apiRequestId}`);
   const method = event.httpMethod;
-  if (event.resource === "/products") {
-    if (method === 'GET') {
-      console.log(`${funcTag} GET /products called`);
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: "Products fetched successfully",
-        })
+  try {
+    let body;
+
+    if (method === 'GET') {
+      if (event.resource === "/products") {
+        console.log(`${funcTag} GET /products called`);
+        const product = await productRepository.getAllProducts();
+        body = {
+          product,
+          message: `Products fetched successfully`,
+          error: null
+        }
+      } else if (event.resource === "/products/{id}") {
+        const productId = event.pathParameters!.id as string
+        console.log(`${funcTag} GET /products/${productId}`);
+        const product = await productRepository.getProductById(productId);
+        body = {
+          product,
+          message: `Product with ID: ${productId} fetched successfully`,
+          error: null
+        };
       };
     };
-  } else if (event.resource === "/products/{id}") {
-    const productId = event.pathParameters!.id as string
-    console.log(`${funcTag} GET /products/${productId}`);
-
     return {
       statusCode: 200,
+      body: JSON.stringify({body})
+    };
+  } catch (error) {
+    console.error(`${funcTag} Error processing request:`, (<Error>error).message);
+    return {
+      statusCode: 404,
       body: JSON.stringify({
-        message: `Product fetched by id: ${productId} successfully`,
+        product: null,
+        message: `Error processing request`,
+        error: (<Error>error).message
       })
     };
-  };
-
-  return {
-    statusCode: 400,
-    body: JSON.stringify({
-      message: "Bad Request: Unsupported method or resource",
-    })
   };
 }

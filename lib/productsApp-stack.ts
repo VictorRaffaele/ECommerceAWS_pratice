78515453476536efc8881e9bc0b60e7642cdeb58
VictorRaffaele <victorraffaele@gmail.com>
2025-06-20 13:18:54 -0300

    feat: create event handler and give permitions

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -5,13 +5,17 @@ import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
 
+interface productsAppStackProps extends cdk.StackProps {
+  eventDdb: dynamodb.Table;
+}
+
 export class ProductsAppStack extends cdk.Stack {
   // Config stack defines the Lambda function for fetching products
   readonly productsFetchHandler: lamdbaNodeJS.NodejsFunction;
   readonly productsAdminHandler: lamdbaNodeJS.NodejsFunction;
   readonly productsDdb: dynamodb.Table;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: productsAppStackProps) {
     super(scope, id, props);
 
     // Define the DynamoDB table for products
@@ -28,6 +32,30 @@ export class ProductsAppStack extends cdk.Stack {
     // Products Layer
     const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn');
     const productsLayer = lamdba.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productsLayerArn);
+
+    const productEventHandler =  new lamdbaNodeJS.NodejsFunction(this, 
+        'ProductsEventsFunction', {
+        runtime: lamdba.Runtime.NODEJS_20_X,
+        functionName: 'ProductsEventsFunction',
+        entry: 'lambda/products/productsEventsFunction.ts',
+        handler: 'handler',
+        memorySize: 512,
+        timeout: cdk.Duration.seconds(2),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+          nodeModules: [
+            'aws-xray-sdk-core',
+          ]
+        },
+        environment: {
+          EVENTS_DDB: props.eventDdb.tableName,
+        },
+        tracing: lamdba.Tracing.ACTIVE,
+        insightsVersion: lamdba.LambdaInsightsVersion.VERSION_1_0_119_0,
+      }
+    );
+    props.eventDdb.grantWriteData(productEventHandler);
 
     // Define the Lambda function that fetches products from DynamoDB
     this.productsFetchHandler = new lamdbaNodeJS.NodejsFunction(this, 
@@ -73,6 +101,7 @@ export class ProductsAppStack extends cdk.Stack {
         },
         environment: {
           PRODUCTS_DDB: this.productsDdb.tableName,
+          PRODUCTS_EVENTS_FUNCTION: productEventHandler.functionName,
         },
         layers: [productsLayer],
         tracing: lamdba.Tracing.ACTIVE,
@@ -81,5 +110,6 @@ export class ProductsAppStack extends cdk.Stack {
     );
     // Grant write permissions to the admin handler
     this.productsDdb.grantWriteData(this.productsAdminHandler);
+    productEventHandler.grantInvoke(this.productsAdminHandler);
   }
 }

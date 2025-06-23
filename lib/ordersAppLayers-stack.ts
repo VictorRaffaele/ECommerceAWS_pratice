b1aadc6252a7a1f79395f13253064123696a042a
VictorRaffaele <victorraffaele@gmail.com>
2025-06-23 17:39:24 -0300

    feat: order stack

---
+++ lib/ordersAppLayers-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+export class OrdersAppLayersStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    const orderLayer = new lambda.LayerVersion(this, 'OrderLayer', {
+      code: lambda.Code.fromAsset('lambda/orders/layers/orderLayer'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      layerVersionName: 'OrderLayer',
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    new ssm.StringParameter(this, 'OrderLayerVersionArn', {
+      parameterName: 'OrderLayerVersionArn',
+      stringValue: orderLayer.layerVersionArn,
+    });
+  }
+}
